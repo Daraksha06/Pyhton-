2072591eb28093e40cbcd3d7464446ee4d20bc4f
Daraksha06 <nasirwaladaraksha@gmail.com>
2025-06-26 11:08:54 +0530

    django

--- django/myapp1/urls.py
+++ django/myapp1/urls.py
@@ -1,7 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path
 from myapp1.views import * 
 urlpatterns = [
-   path("",index,name="index")
+   path("",index,name="index"),
+   path("about", about , name="about"),
+   path("contact",contact,name="contact"),
+   path("service",service,name="service")
+
 ]
--- django/myapp1/views.py
+++ django/myapp1/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index")
+    #return HttpResponse("Index")
+    return render(request,"index.html",{"title":"home"})
+def about(request):
+    return render(request,"about.html",{"title":"about"})
+
+def contact(request):
+    return render(request,"contact.html",{"title":"contact"})
+
+def service(request):
+    return render(request,"service.html",{"title":"service"})
