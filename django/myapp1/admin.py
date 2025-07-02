c40b258defa5f3b63aea4a5d20672ebba12a2735
Daraksha06 <nasirwaladaraksha@gmail.com>
2025-07-02 23:40:02 +0530

    CRUD

--- django/myapp1/admin.py
+++ django/myapp1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from myapp1.models import *
 # Register your models here.
+admin.site.register(Student)
+
+# now makemigrations and migrate after registering the model
