d6d8d652eb0603c56b92c7225f8ed192f2884c1e
Daraksha06 <nasirwaladaraksha@gmail.com>
2025-07-04 01:02:19 +0530

    create done

--- crud_project/myapp/urls.py
+++ crud_project/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from myapp.views import *
 urlpatterns = [
-path('',index ,name="index")
+path('',index ,name="index"),
+path('reg' ,reg , name="reg")
 ]
--- crud_project/myapp/views.py
+++ crud_project/myapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from myapp.models import * #import models
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+def reg(request):
+    if request.method == 'POST':
+        data = request.POST
+        name = data.get('name')
+        email = data.get('email')
+        phone = data.get('phone')
+        age = data.get('age')
+        st = Student.objects.create(name= name , email = email , phone = phone , age = age)
+        if st:
+            return render(request,'index.html', {"msg" : "Registration successful !!"} )
+    return render(request, 'index.html')
